622a7eecb9e28704630dc9cd66bd1d7322e042ec
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-06-25 22:51:56 -0300

    feat: create a helper to format date US for PT-BR

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+
+  #
+  # Create a current date mask EN to PT-BR
+  #
+  # @return [Date] Current date with mask to PT_BR
+  #
+  def date_pt_br(date_us)
+    date_us.strftime("%d/%m/%Y")
+  end
 end
