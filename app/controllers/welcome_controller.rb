9b256d79e5c3126687ee25301531b4f336607a76
luizmoitinho <luizcarlos_costam@hotmail.com>
2021-06-27 00:02:32 -0300

    feat: catch query params and show in the view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,13 @@ class WelcomeController < ApplicationController
     # instance/session variables start with @
     @name = "Luiz Moitinho"
     @course = "Ruby on Rails"
+    if  params.key?('name') && params[:name] != ''
+      @name = params[:name]
+    end
+    if  params.key?('course') && params[:course] != ''
+      @course = params[:course]
+    end
+
+
   end
 end
