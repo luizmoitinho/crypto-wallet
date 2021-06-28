6715a39e9d1db96fdf0669dfe4107cbf91bd3fb4
luizmoitinho <luizcarlos_costam@hotmail.com>
2021-06-28 12:28:32 -0300

    feat: change response destroy coin and label to create a new coin

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -52,7 +52,7 @@ class CoinsController < ApplicationController
   def destroy
     @coin.destroy
     respond_to do |format|
-      format.html { redirect_to coins_url, notice: "Coin was successfully destroyed." }
+      format.html { redirect_to coins_url, notice: "Moeda foi removida com sucesso!" }
       format.json { head :no_content }
     end
   end
